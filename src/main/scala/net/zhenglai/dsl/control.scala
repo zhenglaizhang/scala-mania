b93798bf62b3ded1dc7290458e0326c608529e5e
Zhenglai Zhang <zhenglaizhang@hotmail.com>
2016-08-15 00:04:52 +0800

    Add continue control dsl

--- src/main/scala/net/zhenglai/dsl/control.scala
+++ src/main/scala/net/zhenglai/dsl/control.scala
@@ -1,8 +1,24 @@
 package net.zhenglai.dsl
 
+import scala.annotation.tailrec
+
 /**
   * Created by zhenglai on 8/15/16.
   */
 object control {
 
+  @tailrec
+  def continue(conditional: => Boolean)(action: => Unit): Unit = {
+    if (conditional) {
+      action
+      continue(conditional)(action)
+    }
+  }
+
+
+  var count = 0
+  continue(count < 5) {
+    println(s"at $count")
+    count += 1
+  }
 }
