cba5e6e9a110c8e5febe1d2d19ec2a683dcb2cce
Zhenglai Zhang <zhenglaizhang@hotmail.com>
2016-08-15 23:16:57 +0800

    Add trampoline test code

---
+++ src/main/scala/net/zhenglai/dsl/Trampoline.scala
@@ -0,0 +1,22 @@
+package net.zhenglai.dsl
+
+import scala.util.control.TailCalls._
+
+/**
+  * Created by zhenglai on 8/15/16.
+  */
+object Trampoline {
+
+  def isEven(xs: List[Int]): TailRec[Boolean] =
+    if (xs.isEmpty) done(true) else tailcall(isOdd(xs.tail))
+
+  def isOdd(xs: List[Int]): TailRec[Boolean] =
+    if (xs.isEmpty) done(false) else tailcall(isEven(xs.tail))
+
+
+  for (i <- 1 to 5) {
+    val even = isEven((1 to i).toList).result
+    println(s"$i is even?\t$even")
+  }
+
+}
