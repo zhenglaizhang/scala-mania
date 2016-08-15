51e050d81d327ce4c8187997edb4a7eb3cbffb9d
Zhenglai Zhang <zhenglaizhang@hotmail.com>
2016-08-15 23:32:39 +0800

    Add basic logger implementation

---
+++ src/main/scala/net/zhenglai/logging/Logger.scala
@@ -0,0 +1,20 @@
+package net.zhenglai.logging
+
+/**
+  * Created by zhenglai on 8/15/16.
+  */
+object Logger {
+
+
+  // In Scala, curried functions are defined with multiple argument lists, each with a single argument
+  def log(level: String)(msg: String) = {
+    println(s"$level => $msg")
+  }
+
+  val info = log("INFO") _
+
+  val error = log("ERROR") _
+
+  val warn = log("WARN") _
+
+}
