89edc6cae71a303797c32bdcf0440e313334a89f
Zhenglai Zhang <zhenglaizhang@hotmail.com>
2016-08-15 23:42:40 +0800

    Add lift and unlift test snippets

--- src/main/scala/net/zhenglai/numeric/Transformer.scala
+++ src/main/scala/net/zhenglai/numeric/Transformer.scala
@@ -9,4 +9,18 @@ object Transformer {
   val inverse: PartialFunction[Double, Double] = {
     case d if d != 0.0 => 1.0 / d
   }
+
+
+  // lift the partial function as one that returns an Option
+  val inverseSafe = inverse.lift
+
+
+  inverse(2)
+  inverse(10)
+  inverseSafe(2).get
+  inverseSafe(0)
+
+  // unlift the function returning Option as partial function
+  val inverseOld = Function.unlift(inverseSafe)
+  inverseOld(0)
 }
