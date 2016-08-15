402e48fca468ec78baaadd07d1d8e4ef0ca84872
Zhenglai Zhang <zhenglaizhang@hotmail.com>
2016-08-15 19:01:32 +0800

    Add Op enum

---
+++ src/main/scala/net/zhenglai/dsl/Op.scala
@@ -0,0 +1,26 @@
+package net.zhenglai.dsl
+
+/**
+  * Created by zhenglai on 8/15/16.
+  */
+object Op extends Enumeration {
+
+  type Op = Value
+
+  val EQ = Value("=")
+  val NE = Value("!=")
+  val LTGT = Value("<>")
+  val LT = Value("<")
+  val LE = Value("<=")
+  val GT = Value(">")
+  val GE = Value(">=")
+}
+
+import Op._
+
+// WHERE x op value
+//  op: =, !=, <>, <, <=, > or >=
+case class WhereOp[T](columnName: String, op: Op, value; T)
+
+// WHERE x IN (a, b, c, ...)
+case class WhereIn[T](columnName: String, val1: T, vals: T*)
