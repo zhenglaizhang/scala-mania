a2b85aedee1aa084f1d81d05d722a7dea112eda8
Zhenglai Zhang <zhenglaizhang@hotmail.com>
2016-08-17 17:56:40 +0800

    Future as monadic

--- src/main/scala/net/zhenglai/concurrent/FutureTest.scala
+++ src/main/scala/net/zhenglai/concurrent/FutureTest.scala
@@ -3,12 +3,20 @@ package net.zhenglai.concurrent
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, Future}
+import scala.util.{Failure, Success, Try}
 
 /**
   * Created by zhenglai on 8/17/16.
   */
+
+case class ThatsOdd(i: Int) extends RuntimeException(s"odd $i received")
+
 object FutureTest {
 
+  val doComplete: PartialFunction[Try[String], Unit] = {
+    case s@Success(_) => println(s)
+    case f@Failure(_) => println(f)
+  }
 
   def main(args: Array[String]): Unit = {
     val futures: IndexedSeq[Future[String]] = (0 to 9) map {
@@ -29,6 +37,31 @@ object FutureTest {
     val n = Await.result(f, Duration.Inf)
 
     println(s"Final result n is $n")
+
+
+    println("---------------------")
+    val futures2 = (0 to 9) map {
+      case i if i % 2 == 0 => Future.successful(i.toString)
+      case i => Future.failed(ThatsOdd(i))
+    }
+
+    futures2 foreach (_ onComplete doComplete)
+
+
+    println("---------------------")
+    val futures3: IndexedSeq[Future[String]] = (0 to 9) map {
+      // ForkJoinPool
+      i => Future {
+        val s = i.toString
+        println(s)
+        s
+      }
+    }
+
+    futures3 foreach {
+      f =>
+        f.map(_.toInt * 2).filter(_ > 10).foreach(i => println(s"Processed $i"))
+    }
   }
 
 }
