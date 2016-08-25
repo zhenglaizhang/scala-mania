e71e62cb1be8815590e24e0d7300eea330e96dde
Zhenglai Zhang <zhenglaizhang@hotmail.com>
2016-08-25 23:07:29 +0800

    Add fibonacci implementation

---
+++ src/main/scala/net/zhenglai/numeric/Fibonacci.scala
@@ -0,0 +1,20 @@
+package net.zhenglai.numeric
+
+object fibonacci {
+  def apply(n: Int): Int = ???
+
+  def fib(n: Int): Int = {
+    /*
+    尾递归是指一个递归函数最后一个语句独立引用了自己。在以上的例子里 go(cnt-1,cur,prev + cur)
+     */
+    @annotation.tailrec
+    def go(count: Int, prev: Int, cur: Int): Int = count match {
+      case m if (m < 0) => sys.error("Negative Number Not Allowed!")
+      case 0            => prev
+      case c            => go(count - 1, cur, cur + prev)
+    }
+
+    go(n, 0, 1)
+  }
+
+}
