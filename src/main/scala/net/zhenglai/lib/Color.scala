3305a2ad147ad62e75ad1d91eb088158336b136f
Zhenglai Zhang <zhenglaizhang@hotmail.com>
2016-08-25 18:29:03 +0800

    Add hybrid types notes

---
+++ src/main/scala/net/zhenglai/lib/Color.scala
@@ -0,0 +1,24 @@
+package net.zhenglai.lib
+
+/*
+ our new type Color consists of three values Red, Green and Blue. Those are mapped to singleton objects (serving as the underlying value constructors),since every value is a unique instance of the type.
+ Color is called 'sum type', since all values of that type are clearly expressed by the sum of all value constructors.
+
+
+ */
+sealed trait Color
+
+case object Red extends Color
+
+case object Green extends Color
+
+case object Blue extends Color
+
+
+//sealed abstract class RGBColor
+/*
+called a product type. In that case, we can’t sum up every single value of the given type, simply by enumerating value constructors (while each value constructor’s representing a single value). This time, all values of that type are expressed as the ‘product’  of all different combinations for those three intensity fields within the parameter list of our value constructor! So the constructors parameters can be seen as the single factors of that product.
+
+Of course there are also some hybrid types possible. There, we speak of algebraic datatypes which feature more than one value constructor (sum), while some (or all) of those available value constructors may offer some fields (constructor parameters) for some other datatypes to wrap (product). In fact, the vast majority of algebraic datatypes are expressed as hybrid types and that’s why algebraic datatypes are often described as ‘sum of products types’ (hence algebraic). Those hybrid types will be the main topic for the next episode! So stay tuned …
+ */
+case class RGBColor( red :Int, green :Int, blue :Int ) //extends RGBColor
---
+++ src/main/scala/net/zhenglai/ws/tlp/hybrid-types.sc
@@ -0,0 +1,52 @@
+
+
+/*
+‘sum of products type’.
+
+algebraic datatypes, which doesn’t feature subtype polymorphism, but only combination (you may wanna think composition) of datatypes
+ */
+
+sealed abstract class Shape
+
+// product type
+final case class Circle(radius: Double) extends Shape
+
+// product type
+final case class Rectangle(width: Double, height: Double)
+
+/*
+sum pf product types
+Ok, the range of possible different (read disjointed) sort of shapes is given by the sum of all existing value constructors, that is Circle plus Rectangle.
+ */
+
+val rect = Rectangle(width = 20.0F, height = 10.0F)
+
+
+/*
+Type cosmetics (type synonyms/alias)
+
+You can introduce a synonym for an existing type at any time, in order to give a type a more descriptive name.
+
+We could introduce some naming conventions and point to the importance of picking meaningful names!
+ */
+
+type Width = Double
+type Height = Double
+
+case class Rectangle2(width: Width, height: Height) extends Shape
+
+/*
+Pay attention
+
+we also saw some obstacles while the compiler only checks for the underlying, masked type.
+
+the compiler won’t complain, since it only checks the underlying type Double, which is ok for both constructor parameters (no matter the type synonyms they exhibit).
+ */
+val bad = Rectangle2(10.0F: Height, 20.0F: Width)
+
+
+// nested type composition
+case class Point(x: Int, y: Int)
+
+case class Circle2(center: Point, radius: Double) extends Shape
+
