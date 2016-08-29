2797f3ffff3c95bfeda65b4f4d49feb213b8961a
Zhenglai Zhang <zhenglaizhang@hotmail.com>
2016-08-29 14:36:08 +0800

    Refine NonZero type class

--- src/main/scala/net/zhenglai/test/BindingTest.scala
+++ src/main/scala/net/zhenglai/test/BindingTest.scala
@@ -12,7 +12,7 @@ object Test {
 
   // This looks for a binding 'x' within the same package (test) as this scope.
   def testSamePackage() {
-    println(x)  // 在另外文件的test package. prints: Externally bound x object in package test
+//    println(x)  // 在另外文件的test package. prints: Externally bound x object in package test
   }
 
   // This defines a new scope with an 'x' binding that we can import with a wildcard.
@@ -34,15 +34,12 @@ object Test {
 
   def testExplicitImport() {
     import Explicit.x
-    import Wildcard._
     println(x)  // .x优先于._  prints: Explicit Import x
   }
 
   // This defines an inline binding for x.  Note that with all the imports, there are no ambiguous naming conflicts.
   def testInlineDefinition() {
     val x = "Inline definition x" //即使写在最前，本地binding x还是最优先
-    import Explicit.x
-    import Wildcard._
     println(x)  // prints:  Inline definition x
   }
 }
---
+++ src/main/scala/net/zhenglai/ws/scalaz/non-zero.sc
@@ -0,0 +1,34 @@
+import net.zhenglai.lib.NonZero
+import net.zhenglai.lib.ToNonZeroOps._
+
+10.isNonZero
+0.isNonZero
+(-2).isNonZero
+
+
+// 试试其它即兴类型
+
+implicit val stringNZInstance: NonZero[String] = NonZero.create {
+  case null | "" => false
+  case _         => true
+}
+
+implicit val boolNZInstance: NonZero[Boolean] = NonZero.create(b => b)
+
+implicit def listNZInstance[A]: NonZero[List[A]] = NonZero.create {
+  case Nil => false
+  case _   => true
+}
+
+
+null.asInstanceOf[String].isNonZero
+"".isNonZero
+
+true.isNonZero
+
+false.isNonZero
+
+Nil.asInstanceOf[List[Int]].isNonZero
+List().isNonZero
+List(1, 2, 3).isNonZero
+('a' to 'b').toList.isNonZero
