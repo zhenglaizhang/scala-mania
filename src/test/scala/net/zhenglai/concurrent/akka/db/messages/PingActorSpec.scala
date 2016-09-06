9582f4e00097e57b54616c51c21e1b99bd3e6611
Zhenglai Zhang <zhenglaizhang@hotmail.com>
2016-09-06 14:06:53 +0800

    Refine PingActorSpec

--- src/test/scala/net/zhenglai/concurrent/akka/db/messages/PingActorSpec.scala
+++ src/test/scala/net/zhenglai/concurrent/akka/db/messages/PingActorSpec.scala
@@ -1,6 +1,6 @@
 package net.zhenglai.concurrent.akka.db.messages
 
-import akka.actor.ActorSystem
+import akka.actor.{ActorSystem, Props}
 import akka.pattern.ask
 import akka.util.Timeout
 import org.scalatest.{BeforeAndAfterEach, FunSpecLike, Matchers}
@@ -29,8 +29,14 @@ class PingActorSpec extends FunSpecLike with Matchers with BeforeAndAfterEach {
   describe("Ping-Pong Actor") {
     describe("when Ping is sent") {
       it("should respond with Pong") {
-        // we ask the actor for a response to a message
+        /* we ask the actor for a response to a message
+        This gives us back a placeholder—a Future—that represents the actor's reply. In the actor code, the actor will send a message back to sender(), which we will receive as the response to this future.
+         */
         val future = pingActor ? "Ping"
+
+        /*
+        The Actor is untyped, so we get back a Future[AnyRef]. We call future. mapTo[String] to change the future's type to the expected type of the result.
+         */
         val result = Await.result(future.mapTo[String], 1 second)
         assert(result == "Pong")
       }
@@ -41,7 +47,15 @@ class PingActorSpec extends FunSpecLike with Matchers with BeforeAndAfterEach {
           Await.result(future.mapTo[String], 1 second)
         }
       }
+    }
 
+    describe("when Echo is set as response and Ping is sent") {
+      it("should response with Echo") {
+//        val otherPingActor = system.actorOf(PingActor.props("Echo"))
+        val otherPingActor = system.actorOf(Props(classOf[PingActor], "Echo"))
+        val future = otherPingActor ? "Ping"
+        Await.result(future.mapTo[String], 1 second) should equal("Echo")
+      }
     }
   }
 }
