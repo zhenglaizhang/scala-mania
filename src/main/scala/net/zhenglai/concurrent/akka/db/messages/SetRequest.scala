7fdaaa15a66184492603239a8c26ad2a0899570f
Zhenglai Zhang <zhenglaizhang@hotmail.com>
2016-09-06 15:56:34 +0800

    Add more immutable messages

--- src/main/scala/net/zhenglai/concurrent/akka/db/messages/SetRequest.scala
+++ src/main/scala/net/zhenglai/concurrent/akka/db/messages/SetRequest.scala
@@ -5,10 +5,15 @@ package net.zhenglai.concurrent.akka.db.messages
   * (The Scala case class is serializable.)
   *
   * Combination of both an insert and an update in one, or like the set operation on a Map.
+  *
   * @param key
   * @param value
   */
 case class SetRequest(key: String, value: Object)
+
+case class SetIfNotExistsRequest(key: String, value: Object)
+
 case class GetRequest(key: String)
+
 case class KeyNotFoundException(key: String) extends Exception
 
