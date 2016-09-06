e18dfb8ce0661a31e5a460906cef2a08846972fc
Zhenglai Zhang <zhenglaizhang@hotmail.com>
2016-09-06 14:30:40 +0800

    Add more about feature test

--- src/test/scala/net/zhenglai/concurrent/akka/db/messages/PingActorSpec.scala
+++ src/test/scala/net/zhenglai/concurrent/akka/db/messages/PingActorSpec.scala
@@ -1,12 +1,12 @@
 package net.zhenglai.concurrent.akka.db.messages
 
-import akka.actor.{ActorSystem, Props}
+import akka.actor.{ActorRef, ActorSystem, Props}
 import akka.pattern.ask
 import akka.util.Timeout
 import org.scalatest.{BeforeAndAfterEach, FunSpecLike, Matchers}
 
-import scala.concurrent.Await
 import scala.concurrent.duration._
+import scala.concurrent.{Await, Future}
 
 class PingActorSpec extends FunSpecLike with Matchers with BeforeAndAfterEach {
 
@@ -23,6 +23,10 @@ class PingActorSpec extends FunSpecLike with Matchers with BeforeAndAfterEach {
   implicit val timeout = Timeout(5 seconds)
 
   val pingActor = system.actorOf(PingActor.props("Pong"))
+
+
+  def askThat(actor: ActorRef, message: String): Future[String] = (actor ? message).mapTo[String]
+
   /*
   This basic pattern can be built on for unit testing Actors synchronously.
    */
@@ -33,6 +37,8 @@ class PingActorSpec extends FunSpecLike with Matchers with BeforeAndAfterEach {
         This gives us back a placeholder—a Future—that represents the actor's reply. In the actor code, the actor will send a message back to sender(), which we will receive as the response to this future.
 
         Asking an actor for a response demonstrates how to talk to an actor from outside the actor system by getting a response via a future.
+
+        Scala future requires a timeout (specied via implicit parameters in the ask method), so the future will fail if the timeout is violated.
          */
         val future = pingActor ? "Ping"
 
@@ -60,6 +66,39 @@ class PingActorSpec extends FunSpecLike with Matchers with BeforeAndAfterEach {
         val otherPingActor = system.actorOf(Props(classOf[PingActor], "Echo"))
         val future = otherPingActor ? "Ping"
         Await.result(future.mapTo[String], 1 second) should equal("Echo")
+      }
+    }
+
+
+    describe("Future example") {
+      import scala.concurrent.ExecutionContext.Implicits.global
+
+      it("should print to console") {
+
+        /*
+        Note that onSuccess takes a partial function, so it  ts well with the Akka untyped responses—pattern matching takes care of determining the type of the result.
+         */
+        (pingActor ? "Ping").onSuccess {
+          case x: String => assert(x == "Pong")
+        }
+
+
+        /*
+        The most common use case is the need to transform a response asynchronously before doing something with it
+        TODO: bugfix
+        These will give you back new Futures of type Char.
+         */
+        (pingActor ? "Ping").map {
+          case x: String => x.charAt(0)
+        } onSuccess {
+          case x: Char => assert(x == 'P')
+        }
+
+        (askThat(pingActor, "Ping")).onSuccess {
+          case x: String => assert(x == "Pong")
+        }
+
+        Thread.sleep(100)
       }
     }
   }
