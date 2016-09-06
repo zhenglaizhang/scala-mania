c5a259a60e84e774dac427e420caa50bdbb54049
Zhenglai Zhang <zhenglaizhang@hotmail.com>
2016-09-06 13:22:35 +0800

    Add EchoActor

---
+++ src/main/scala/net/zhenglai/concurrent/akka/db/messages/EchoActor.scala
@@ -0,0 +1,28 @@
+package net.zhenglai.concurrent.akka.db.messages
+
+import akka.actor.Actor
+
+class EchoActor extends Actor {
+
+
+  import EchoActor._
+
+  var lastValue: Object = _
+
+  override def receive: Receive = {
+    case Store(value: Object) =>
+      lastValue_=(value)
+    case EchoActor.Read         =>
+      sender ! Result(lastValue)
+  }
+}
+
+object EchoActor {
+
+  case class Store(value: Any)
+
+  case class Result(value: Any)
+
+  case object Read
+
+}
--- src/test/scala/net/zhenglai/concurrent/akka/db/messages/EchoActorSpec.scala
+++ src/test/scala/net/zhenglai/concurrent/akka/db/messages/EchoActorSpec.scala
@@ -2,9 +2,10 @@ package net.zhenglai.concurrent.akka.db.messages
 
 import akka.actor.ActorSystem
 import akka.testkit.TestActorRef
+import net.zhenglai.concurrent.akka.db.messages.EchoActor.Store
 import org.scalatest.{BeforeAndAfterEach, FunSpecLike, Matchers}
 
-class AkkademyDbSpec extends FunSpecLike with Matchers with BeforeAndAfterEach {
+class EchoActorSpec extends FunSpecLike with Matchers with BeforeAndAfterEach {
 
   /**
     *  get a reference to an actor system
@@ -19,23 +20,31 @@ class AkkademyDbSpec extends FunSpecLike with Matchers with BeforeAndAfterEach {
   /*
   This basic pattern can be built on for unit testing Actors synchronously.
    */
-  describe("akkademyDb") {
-    describe("given SetRequest") {
-      it("should place key/value into map") {
+  describe("echoActor") {
+    describe("given Store") {
+      it("should remember the value") {
         // use Akka Testkit to create a TestActorRef which has a synchronous API, and lets us get at the underlying actor.
         /**
           * Actor instances are hidden away so the act of creating an actor in our actor system returns an ActorRef
           */
-        val actorRef = TestActorRef(new AkkademyDb)
+        val actorRef = TestActorRef(new EchoActor)
 
         /*
         Because we are using TestActorRef, the call to tell will not continue until the request is processed.
         tell is an asynchronous operation that returns immediately in normal usage.
          */
-        actorRef ! SetRequest("key", "value")
+        actorRef ! Store("value")
         val akkademyDb = actorRef.underlyingActor
-        akkademyDb.map.get("key") should equal(Some("value"))
-        akkademyDb.map.get("notexist") should not equal(Some("value"))
+        akkademyDb.lastValue should equal("value")
+      }
+
+
+      it("should remember only the last value") {
+        val actorRef = TestActorRef(new EchoActor)
+
+        actorRef ! Store("v1")
+        actorRef ! Store("v2")
+        actorRef.underlyingActor.lastValue should equal ("v2")
       }
     }
   }
