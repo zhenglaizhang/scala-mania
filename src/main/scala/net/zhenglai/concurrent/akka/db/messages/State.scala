8b0eeed96a04ecd1b518abe97c73034f638d614a
Zhenglai Zhang <zhenglaizhang@hotmail.com>
2016-09-08 15:10:31 +0800

    Akka FSM

---
+++ src/main/scala/net/zhenglai/concurrent/akka/db/messages/State.scala
@@ -0,0 +1,36 @@
+package net.zhenglai.concurrent.akka.db.messages
+
+/**
+  * For our FSMs, we will improve on the hotswap example to store messages in the actor instead of stashing them:
+  *
+  * • Disconnected: Not online and no messages are queued
+  * • Disconnected and Pending: Not online and messages are queued
+  * • Connected: Online and no messages are queued
+  * • Connected and Pending: Online and Messages are Pending
+  *
+  */
+sealed trait State {
+
+
+
+}
+
+case object Disconnected extends State
+
+case object Connected extends State
+
+case object ConnectedAndPending extends State
+
+class Request {}
+
+object StateContainerTypes {
+  /**
+    * For our state container, we'll store a list of requests to be processed on a  ush event:
+    */
+  type RequestQueue = List[Request]
+}
+
+
+class Flush {}
+
+case object Flush
