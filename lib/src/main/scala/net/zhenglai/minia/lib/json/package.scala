870f844d04c9ea91cbe47b322afa415b352ab07c
Zhenglai Zhang <zhenglai.zhang@vpon.com>
2016-11-26 17:34:01 +0800

    refactor json show

--- lib/src/main/scala/net/zhenglai/minia/lib/json/package.scala
+++ lib/src/main/scala/net/zhenglai/minia/lib/json/package.scala
@@ -3,21 +3,16 @@ package net.zhenglai.minia.lib
 package object json {
   def show(json: JSON): String = json match {
     case JSeq(elems) =>
-      s"[${elems.map(show(_)).mkString(", ")}]"
+      "[" + (elems map show mkString ", ") + "]"
     case JObj(bindings) =>
-      s"{ ${
-        bindings.map {
-          case (key, value) =>
-            key + ": " + show(value)
-        }.mkString(", ")
-      } }"
-    case JNum(num) =>
-      s"$num"
-    case JBool(bool) =>
-      bool.toString
-    case JStr(str) =>
-      s""""$str""""
-    case JNull =>
-      "null"
+      val assocs = bindings map {
+        case (key, value) => "\"" + key + "\":" + show(value)
+      }
+
+      "{" + (assocs mkString ", ") + "}"
+    case JNum(num) => num.toString
+    case JBool(bool) => bool.toString
+    case JStr(str) => "\"" + str + "\""
+    case JNull => "null"
   }
 }
