1f6b048a648e1c8752823e9c6d11ca149af4b55e
Zhenglai Zhang <zhenglai.zhang@vpon.com>
2016-11-28 23:59:16 +0800

    add accounting

---
+++ ws/src/main/scala/net/zhenglai/accounting/Account.scala
@@ -0,0 +1,38 @@
+package net.zhenglai.accounting
+
+import java.util.{ Calendar, Date }
+
+import scala.util.{ Failure, Success, Try }
+
+import net.zhenglai.accounting.AccountService.Amount
+
+object AccountService extends AccountService {
+  def today = Calendar.getInstance.getTime
+
+  type Amount = BigDecimal
+}
+
+case class Balance(amount: Amount = 0)
+
+case class Account(
+  no: String,
+  name: String,
+  dateOfOpening: Date,
+  balance: Balance = Balance()
+)
+
+trait AccountService {
+
+  def debit(account: Account, amount: Amount): Try[Account] = {
+    if (account.balance.amount < amount) {
+      Failure(new Exception("Insufficient balance in account"))
+    } else {
+      Success(account.copy(balance = Balance(account.balance.amount - amount)))
+    }
+  }
+
+  def credit(account: Account, amount: Amount): Try[Account] = {
+    Success(account.copy(balance = Balance(account.balance.amount + amount)))
+  }
+}
+
